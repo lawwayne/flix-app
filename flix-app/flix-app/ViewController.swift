2d5db15a88f51a5343849bdbe36943daa3adb621
lawwayne <72894553+lawwayne@users.noreply.github.com>
2022-02-13 21:53:09 -0500

    commit changes

---
+++ flix-app/flix-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix-app
+//
+//  Created by Kazi Faiaz on 2/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
